050eb8545c8f04474f1aedf46e02ce67b74524ed
Ola Aboelmagd <ola.rashad00@gmail.com>
2025-07-31 05:02:30 +0300

    phase 1&2&3 completed

--- Todo-List-nodejs/index.js
+++ Todo-List-nodejs/index.js
@@ -103,4 +103,6 @@ app.listen(port,(err) => {
         console.log(`Error: ${err}`);
     }
     console.log(`Yupp! Server is running on port ${port}`);
+    console.log(" new version is live");
 })
+
